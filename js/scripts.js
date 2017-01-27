626d6cc936a3bfe469ee050b9b35e93a7b77aefb
Kamil Rajca <kamil.rajca@gmail.com>
2017-01-27 14:40:37 +0100

    moduł 8.4 Pole trójkąta

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+// 8.4 pole trójkąta
+var a = 2,
+    h = 4 ,
+    triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log(triangleArea);
+
+// 8.4 pole trójkąta z promt
+var a = prompt ('podaj długośc podstawy trójkąta a:');
+var h = prompt ("podaj wysokość trójkąta h:");
+var triangleArea = a * h / 2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log(triangleArea);
