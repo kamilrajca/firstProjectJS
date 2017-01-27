2e5e30b34106a27d14aae953a6339fbaf54969d6
Kamil Rajca <kamil.rajca@gmail.com>
2017-01-27 16:04:09 +0100

    Instrukcje warunkowe

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-// plik scrips.js
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+// instrukcje warunkowe 
+var a = 5;
+var b = 4;
+value = (a*a) + (2 * a * b) - (b*b);
+
+if (value > 0) {
+  alert('Wynik dodatni')
+} else if (value < 0) {
+  alert('Wynik ujemny')
+} else {
+  alert('Wynik zerowy')
+}
