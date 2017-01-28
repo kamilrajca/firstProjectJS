a151369bedb99cd0f7e6ad20712f52b12bc4dce2
Kamil Rajca <kamil.rajca@gmail.com>
2017-01-28 11:01:18 +0100

    clear lert to return

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function getTriangleArea(a, h) {
   if ( (a > 0) && (h > 0) ) {
     return a*h/2;
   } else {
-     alert('Nieprawidłowe dane');
+      return 'Nieprawidłowe dane';
     }
 }
 console.log( getTriangleArea(a, h) );
