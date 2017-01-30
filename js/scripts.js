443a77270351300396b745b12888470b2b8ab4ad
Kamil Rajca <kamil.rajca@gmail.com>
2017-01-30 19:16:44 +0100

    lancuchy znakow

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-// plik scrips.js
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+// zadanie 8.8 łąńcuchy znaków
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase();
+
+console.log(animalUpperCased);
+
+var textCharsAfter = text.replace('Papugi', animalUpperCased);
+console.log(textCharsAfter.length/2);
+
+var partOftextCharsAfter = textCharsAfter.slice(0,45);
+console.log(partOftextCharsAfter);
