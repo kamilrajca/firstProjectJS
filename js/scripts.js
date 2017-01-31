8b7fa4c8ae80b8051dd443d3fd731d11f5f233b7
Kamil Rajca <kamil.rajca@gmail.com>
2017-01-31 22:43:10 +0100

    dodanie choinki wysrodkowanej

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,23 @@ rysujChoinke(4);
 console.log();
 rysujChoinke(5);
 console.log();
+
+// choinka z spacją 
+
+function rysujChoinke(poziom) {
+  for(var i=0; i <= poziom; i++) {
+    var star = '';
+
+    for (var j=0; j < poziom- i; j++){
+      star += ' ';
+    }
+
+    for(var n=0; n<(2*i-1); n++) {
+      star += '*';
+    }
+    console.log(star)
+  }
+}
+
+rysujChoinke(5);
+console.log();
