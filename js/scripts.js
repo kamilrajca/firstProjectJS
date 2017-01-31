cfc1796789889eeb52c2e501a83fa054dc4353af
Kamil Rajca <kamil.rajca@gmail.com>
2017-01-31 22:13:12 +0100

    clear lenght

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// zadanie 8.8 łąńcuchy znaków
+// zadanie 8.8 łańcuchy znaków
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 
 var animal = 'Zielone słonie';
@@ -7,7 +7,8 @@ var animalUpperCased = animal.toUpperCase();
 console.log(animalUpperCased);
 
 var textCharsAfter = text.replace('Papugi', animalUpperCased);
-console.log(textCharsAfter.length/2);
 
-var partOftextCharsAfter = textCharsAfter.slice(0,45);
+var textshort = (textCharsAfter.length/2);
+
+var partOftextCharsAfter = textCharsAfter.slice(0, textshort);
 console.log(partOftextCharsAfter);
