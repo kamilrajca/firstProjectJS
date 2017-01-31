de2c04718123ec8389a433639e704e2fc534043c
Kamil Rajca <kamil.rajca@gmail.com>
2017-01-31 08:43:50 +0100

    choinka

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-// plik scrips.js
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+// zadanie 8.9 choinka
+
+function rysujChoinke(poziom){ 
+  for (var i=0; i<=poziom; i++) {
+    var star = "";
+    for (var n=0; n <(2*i-1); n++) {
+        star += '*';
+    }
+    console.log(star)
+  }
+} 
+rysujChoinke(1);
+console.log();
+rysujChoinke(2);
+console.log();
+rysujChoinke(3);
+console.log();
+rysujChoinke(4);
+console.log();
+rysujChoinke(5);
+console.log();
